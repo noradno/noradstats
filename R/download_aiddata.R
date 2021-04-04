575bf8398294fbc9ead335268dd3f7de00b27877
Einar Tornes <einartornes86@outlook.com>
2021-04-04 23:46:46 +0200

    possibility to easily download to and read from sub folder "data/"

--- R/download_aiddata.R
+++ R/download_aiddata.R
@@ -1,7 +1,8 @@
 #' Download file from (noradstats) Google Drive
 #'
-#' @param gmail_account Google Drive account. Default: noradstats@gmail.com
 #' @param file Character string of desired file to download. Default: \emph{statsys_10yr.csv}. Use noradstats::find_file() to find available files.
+#' @param gmail_account Google Drive account. Default: noradstats@gmail.com
+#' @param subdir If TRUE, the file is stored in a sub folder \emph{data}. If FALSE, the file is stored in the current directory.
 #'
 #' @return Downloads and saves file in working directory.
 #' @export
@@ -9,9 +10,34 @@
 #' @examples
 #' ?download_aiddata()
 #'
-download_aiddata <- function(file = "statsys_10yr.csv", gmail_account = "noradstats@gmail.com") {
+download_aiddata <- function(file = "statsys_10yr.csv",
+                             gmail_account = "noradstats@gmail.com",
+                             subdir = FALSE) {
+  
+  # Authenticating Google drive connection
   googledrive::drive_auth(email = gmail_account)
-  googledrive::drive_download(file = file,
-                              path = file,
-                              overwrite = TRUE)
+  
+  # If storing file in current directory
+  if (subdir == FALSE) {
+    googledrive::drive_download(file = file,
+                                path = file,
+                                overwrite = TRUE)
+    
+    # If storing in existing sub directory ("data")
+  } else if (subdir == TRUE & file.exists("./data") == TRUE) {
+    googledrive::drive_download(
+      file = file,
+      path = paste0("data/", file),
+      overwrite = TRUE
+    )
+    
+    # If storing in new sub directory ("data")
+  } else {
+    dir.create(file.path("./data"))
+    googledrive::drive_download(
+      file = file,
+      path = paste0("data/", file),
+      overwrite = TRUE
+    )
+  }
 }
--- R/read_aiddata.R
+++ R/read_aiddata.R
@@ -1,6 +1,7 @@
 #' Read delimited Norwegian Development Aid dataset (Statsys format and column names) into a tibble
 #'
 #' @param file Path to local csv-file of Norwegian Development Aid (Statsys format and column names). Default: \emph{statsys_10yr.csv}
+#' @param subdir If TRUE, look for file in sub folder \emph{data}. If FALSE, look in current directory as specified in \emph{file} argument.
 #'
 #' @return Returns a tibble using the vroom package, using delim = ";" and num_threads = 1 (to avoid splitting strings into new rows)
 #' @export
@@ -8,8 +9,18 @@
 #' @examples
 #' ?read_aiddata()
 #'
-read_aiddata <- function(file = "statsys_10yr.csv") {
-  data <- vroom::vroom(file,
-                       delim = ";", # Semi-column separated
-                       num_threads = 1) # Avoid that special characters make new rows
+read_aiddata <- function(file = "statsys_10yr.csv", subdir = FALSE) {
+  
+  # Read file from current directory
+  if (subdir == FALSE) {
+    data <- vroom::vroom(file,
+                         delim = ";", # Semi-column separated
+                         num_threads = 1) # Avoid special characters make new rows
+    
+    # Read file from sub folder "data"
+  } else {
+    data <- vroom::vroom(paste0("data/", file),
+                         delim = ";",
+                         num_threads = 1)
+  }
 }
