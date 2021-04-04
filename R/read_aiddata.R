4463ffed3740f1b54fcaa8157dfbf250ac7e3ff4
Einar Tornes <einartornes86@outlook.com>
2021-04-04 17:58:07 +0200

    removed decimal_mark-specification

--- R/read_aiddata.R
+++ R/read_aiddata.R
@@ -2,17 +2,14 @@
 #'
 #' @param file Path to local csv-file of Norwegian Development Aid (Statsys format and column names). Default: \emph{statsys_10yr.csv}
 #'
-#' @return Returns a tibble using the vroom package, using delim = ";", comma as decimal, and num_threads = 1.
+#' @return Returns a tibble using the vroom package, using delim = ";" and num_threads = 1 (to avoid splitting strings into new rows)
 #' @export
 #'
 #' @examples
 #' ?read_aiddata()
 #'
 read_aiddata <- function(file = "statsys_10yr.csv") {
-  locale <- readr::locale(decimal_mark = ",") # Set comma as decimal mark
-
   data <- vroom::vroom(file,
                        delim = ";", # Semi-column separated
-                       locale = locale,
                        num_threads = 1) # Avoid that special characters make new rows
 }
