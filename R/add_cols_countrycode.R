d655b407c02105bf4e4e08132cc27b1e79ba2d31
einartornes <45436606+einartornes@users.noreply.github.com>
2024-01-14 14:05:48 +0100

    fixing the add_cols_countrycode.

--- R/add_cols_countrycode.R
+++ R/add_cols_countrycode.R
@@ -1,10 +1,10 @@
-#' Add country code columns to dataframe
+#' Add ISO3 country code columns to dataframe
 #'
-#' @param data Input dataframe of Norwegian development assistance
+#' @param data Input data frame of Norwegian development assistance, with column \emph{Recipient country} containing country names.
 #'
-#' @return Returns dataframe with additional columns:
+#' @return Returns data frame with additional column:
 #' \itemize{
-#'   \item ISO3: ISO-3 character code, identified using package \emph{countrycode} based on column \emph{Recipient country}. Non-matches are given NA values and are returned in a warning message. 
+#'   \item iso3: iso3 character code, identified using package \emph{countrycode} based on column \emph{Recipient country}. Non-matches are given NA values and are returned in a warning message. 
 #' }
 #'
 #' @export
@@ -13,9 +13,9 @@
 
 add_cols_countrycode <- function(data) {
   
-  # Data frame of unique countries, by removing observations with unspecified income category
+  # Data frame of unique countries, by removing global, regional, adm. Not using Income category to filtering as the variable was introduced in 1999.
   df_unique_countries <- data |> 
-    dplyr::filter(`Income category` != "Unspecified") |> 
+    dplyr::filter(!stringr::str_detect(`Recipient country`, "Regional|regional|Multilateral|Global|Administration")) |> 
     dplyr::select(`Recipient country`) |> 
     dplyr::arrange(`Recipient country`) |> 
     unique()
@@ -23,7 +23,8 @@ add_cols_countrycode <- function(data) {
   # Add column iso3 character code to the data frame of unique countries. Non-matches are given NA values and are returned in a warning message. 
   df_unique_countries <- df_unique_countries |> 
     dplyr::mutate(iso3 = countrycode::countrycode(`Recipient country`, origin = "country.name", destination = "iso3c",
-                                                  custom_match = c("Kosovo" = "XKX")))
+                                                  custom_match = c("Kosovo" = "XKX",
+                                                                   "Yemen Dem. Rep." = "YEM")))
   
   # Include column ISO3 in the original data frame
   data <- dplyr::left_join(data, df_unique_countries, by = "Recipient country")
