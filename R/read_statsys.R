9d554fd6f999626db47d81463dab3465adf43e95
Tornes <einar.tornes@norad.no>
2024-05-15 22:13:58 +0200

    fix read_statsys()

--- R/read_statsys.R
+++ R/read_statsys.R
@@ -1,18 +1,17 @@
-#' Access database of Statsys data from R
+#' Read Statsys data into R
 #'
-#' This function creates a proxy tibble connected to the Statsys data table in the DuckDB database.
+#' This function imports all data from the statsys table in the DuckDB database.
 #' This data includes Norwegian official development assistance (ODA), ODA frame agreement level data, Other official flows(OOF), export credits and private flows.
 #' The data covers 1960 to recent year.
 #' The DuckDB database file is located on Norads Microsoft Sharepoint site and is expected to be synced via Microsoft Teams to to the users local directory.
-#' Use DBI::dbDisconnect(con, shutdown=TRUE) to close connection to database.
 #'
-#' @return Returns a proxy tibble connected to a Statsys table in the DuckDB database.
+#' @return Returns a tibble of ODA, OOF and PF data from the statsys table in the DuckDB database.
 #' @export
 #' @examples
-#' ?access_statsys()
+#' ?read_statsys()
 #'
 
-access_statsys <- function() {
+read_statsys <- function() {
   
   # Specify user specific path to database file
   docs <- path.expand("~")
@@ -26,11 +25,16 @@ access_statsys <- function() {
   default_path_end <- "/Norad/Norad-Avd-Kunnskap - Statistikk og analyse/06. Statistikkdatabaser/3. Databasefiler/statsys.duckdb"
   default_path <- paste0(home, default_path_end)
   
-  # Connect to the database table
+  # Connect to the statsys table in the DuckDB database
   con <- DBI::dbConnect(duckdb::duckdb(), default_path)
   
-  # Create a proxy tibble connected to the Statsys table in the DuckDB database.
-  df_remote_statsys <- dplyr::tbl(con, "statsys")
+  # Read all data from the statsys table
+  df_statsys <- con |>
+    DBI::dbReadTable("statsys") |>
+    tibble::as_tibble()
   
-  return(df_remote_statsys)
+  # Disconnect database
+  DBI::dbDisconnect(con, shutdown = TRUE)
+  
+  return(df_statsys)
 }
