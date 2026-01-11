70156fa66f2a429de2a2e34d70225fcad5d46bec
Einar Tornes <einartornes@outlook.com>
2026-01-11 12:56:07 +0100

    Fix agreement_period, agreement_period_from, agreement_period_to

--- R/read_pta_agreement_totals_from_csv.R
+++ R/read_pta_agreement_totals_from_csv.R
@@ -30,11 +30,24 @@ read_pta_agreement_totals_from_csv <- function(path) {
     dplyr::mutate(
       # Convert thousands to NOK
       expected_agreement_total = expected_agreement_total * 1000,
-      # Extract from/to years from agr_period column
-      agreement_period_from = stringr::str_sub(agr_period, end = 4),
-      agreement_period_to = stringr::str_sub(agr_period, start = -4)
+      # Cleaning the agreement period variable (extract the years, allow NA years from to, and create additional from and to variable)
+      agreement_period = {
+        x <- stringr::str_squish(as.character(agr_period))
+        x <- stringr::str_replace_all(x, "\\s*-\\s*", "-")
+        
+        from <- stringr::str_extract(x, "^\\d{4}")
+        to   <- stringr::str_extract(x, "(?<=-)\\d{4}$")
+        
+        dplyr::if_else(
+          !is.na(from) | !is.na(to),
+          paste0(dplyr::coalesce(from, "NA"), "-", dplyr::coalesce(to, "NA")),
+          NA_character_
+        )
+      },
+      agreement_period_from = as.integer(stringr::str_extract(agreement_period, "^\\d{4}")),
+      agreement_period_to   = as.integer(stringr::str_extract(agreement_period, "(?<=-)\\d{4}$"))
     ) |>
-    dplyr::rename(agreement_period = agr_period) |>
+    dplyr::select(-agr_period) |> 
     dplyr::relocate(expected_agreement_total, .after = dplyr::last_col())
   
   return(df)
